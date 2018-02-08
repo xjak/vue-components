479c02abec435d246199c086a2c11ce1df017091
JiangKai <jiangkai@zyw18.com>
2018-02-08 18:10:15 +0800

    自定义键盘完善功能

--- src/components/keyboard.vue
+++ src/components/keyboard.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
 	<div class="keyboard-input" ref="input">
-		<span class="tip-text" :style="{color:tipColor}">{{tipText}}</span>{{value}}<span class="keyboard-cursor" ref="cursor">|</span>
+		{{frontValue}}<span class="keyboard-cursor" ref="cursor">|</span>{{backValue}}<span class="tip-text" :style="{color:tipColor}" v-if="!(frontValue||backValue)">{{tipText}}</span>
 	</div>
 	<div class="keyboard" ref="keyboard">
 		<div class="keyboard-content" ref="keyboardContent">
@@ -60,115 +60,208 @@ export default {
 	data() {
 		return {
 			tipText: this.title,
-			tipColor: '#ccc',
-			value: '',
-			tipValue: ''
+			tipColor: '#dcdcdc',
+			frontValue: '',
+			backValue: '',
+			tipValue: '',
+			closeKeyboard: null,
+			showKeyboard: null,
+			flag: 1
 		}
 	},
-	props: ['title'],
-	mounted() {
-		let _this = this
-		let input = this.$refs.input
-		let cursor = this.$refs.cursor
-		let keyboard = this.$refs.keyboard
-		let content = this.$refs.keyboardContent
-		let btnList = this.$refs.keyboard.querySelectorAll('li')
-		let tip = this.$refs.tip
-		let tipTime = null
-		let cursorTime = null
-		let closeKeyboard = () => {
-			content.style.transform = 'translateY(200px)'
-			clearInterval(cursorTime)
-			setTimeout(() => {
-				keyboard.style.display = 'none'
-			}, 300)
-			cursor.style.opacity = 0
-			this.tipText = this.value === '' ? this.title : ''
-			this.$emit('change', this.value)
+	props: ['title', 'length', 'initValue', 'show', 'hide'],
+	watch: {
+		initValue() {
+			if (this.flag) this.frontValue = this.initValue + ''
+		},
+		show() {
+			if (this.show) this.showKeyboard()
+		},
+		hide() {
+			if (this.hide) this.closeKeyboard()
 		}
-		keyboard.addEventListener('click', closeKeyboard, false)
-		content.addEventListener('touchend', (e) => {
-			e.preventDefault()
-		}, false)
-		input.addEventListener('click', () => {
-			keyboard.style.display = 'block'
-			setTimeout(() => {
-				content.style.transform = 'translateY(0)'
-			}, 10)
-			clearInterval(cursorTime)
-			cursorTime = setInterval(() => {
-				if (window.getComputedStyle(cursor, null).opacity === '1') {
-					cursor.style.opacity = 0
-				} else {
-					cursor.style.opacity = 1
-				}
-			}, 500)
-			if (cursorTime) _this.tipText = ''
-		}, false)
-		for (let i = 0; i < btnList.length; i++) {
-			btnList[i].addEventListener('touchstart', function (e) {
-				e.preventDefault()
-				if (this.innerText === 'Done') return
-				let x = this.offsetLeft
-				let y = this.offsetTop - 45
-				_this.tipValue = this.innerText
-				tip.style.transform = 'translate(' + x + 'px,' + y + 'px)'
-				tip.style.display = 'block'
-				if (this.innerText !== '×') {
-					this.style.color = '#fff'
-					this.style.backgroundColor = '#aaa'
-				}
-			}, false)
-			btnList[i].addEventListener('touchend', function (e) {
-				e.preventDefault()
-				clearTimeout(tipTime)
-				tipTime = setTimeout(() => {
-					tip.style.display = 'none'
+	},
+	methods: {
+		getValue() {
+			return this.frontValue + this.backValue
+		},
+		addEvent() {
+			if (this.initValue) this.frontValue = this.initValue + ''
+			let _this = this
+			let input = this.$refs.input
+			let cursor = this.$refs.cursor
+			let keyboard = this.$refs.keyboard
+			let content = this.$refs.keyboardContent
+			let btnList = this.$refs.keyboard.querySelectorAll('li')
+			let tip = this.$refs.tip
+			let tipTime = null
+			let cursorTime = null
+			let tocuhTime = 0
+			this.closeKeyboard = () => {
+				content.style.transform = 'translateY(200px)'
+				clearInterval(cursorTime)
+				cursorTime = null
+				setTimeout(() => {
+					keyboard.style.display = 'none'
 				}, 300)
-				switch (this.innerText) {
-					case '×':
-						_this.value = _this.value.slice(0, -1)
-					break
-					case 'Done':
-						closeKeyboard()
-					break
-					default:
-						_this.value += this.innerText
-						this.style.color = '#333'
-						this.style.backgroundColor = '#fff'
+				this.frontValue = this.getValue()
+				this.backValue = ''
+				cursor.style.opacity = 0
+				this.$emit('blur', this.frontValue)
+			}
+			this.showKeyboard = () => {
+				let inputs = document.querySelectorAll('input')
+				for (let i = 0; i < inputs.length; i++) {
+					inputs[i].blur()
+				}
+				// 自动滚动到可视区域
+				let top = input.offsetTop
+				let obj = input.offsetParent
+				while (obj) {
+					if (obj) top += obj.offsetTop
+						obj = obj.offsetParent
+				}
+				top = top - document.body.scrollTop
+				let wH = window.innerHeight - 260
+				if (top > wH) {
+					document.body.scrollTop = Math.abs(wH - top)
+				}
+				keyboard.style.display = 'block'
+				setTimeout(() => {
+					content.style.transform = 'translateY(0)'
+				}, 10)
+				cursorTime = setInterval(() => {
+					if (window.getComputedStyle(cursor, null).opacity === '1') {
+						cursor.style.opacity = 0
+					} else {
+						cursor.style.opacity = 1
+					}
+				}, 500)
+				_this.$emit('focus', _this.getValue())
+			}
+			document.getElementById('app').addEventListener('click', this.closeKeyboard, false)
+			content.addEventListener('touchend', (e) => {
+				e.preventDefault()
+			}, false)
+
+			input.addEventListener('touchstart', (e) => {
+				tocuhTime = Date.now()
+				if (cursorTime) {
+					let inputLeft = input.offsetLeft
+					let inputObj = input.offsetParent
+					while (inputObj) {
+						if (inputObj) inputLeft += inputObj.offsetLeft
+							inputObj = inputObj.offsetParent
+					}
+					let focusX = Math.round((e.targetTouches[0].clientX.toFixed(0) - 5 - inputLeft) / 10)
+					let text = this.frontValue + this.backValue
+					if (text) {
+						this.frontValue = text.slice(0, focusX)
+						this.backValue = text.slice(focusX)
+					}
+				}
+			})
+			input.addEventListener('touchend', (e) => {
+				e.preventDefault()
+				if ((Date.now() - tocuhTime < 300) && !cursorTime) {
+					this.flag = 0
+					this.showKeyboard()
 				}
 			}, false)
+			let deleteTimeOut = null
+			for (let i = 0; i < btnList.length; i++) {
+				btnList[i].addEventListener('touchstart', function (e) {
+					e.preventDefault()
+					if (this.innerText === 'Done') return
+					let x = this.offsetLeft
+					let y = this.offsetTop - 45
+					_this.tipValue = this.innerText
+					tip.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+					tip.style.display = 'block'
+					if (this.innerText !== '×') {
+						this.style.color = '#fff'
+						this.style.backgroundColor = '#aaa'
+					} else {
+						// 持续删除
+						deleteTimeOut = setTimeout(() => {
+							let deleteValue = () => {
+								_this.frontValue = _this.frontValue.slice(0, -1)
+								if (deleteTimeOut) setTimeout(deleteValue, 70)
+							}
+							deleteValue()
+						}, 300)
+					}
+				}, false)
+				btnList[i].addEventListener('touchend', function (e) {
+					e.preventDefault()
+					clearTimeout(tipTime)
+					clearTimeout(deleteTimeOut)
+					deleteTimeOut = null
+					tipTime = setTimeout(() => {
+						tip.style.display = 'none'
+					}, 300)
+					switch (this.innerText) {
+						case '×':
+							_this.frontValue = _this.frontValue.slice(0, -1)
+							break
+						case 'Done':
+							_this.closeKeyboard()
+							break
+						default:
+							this.style.color = '#333'
+							this.style.backgroundColor = '#fff'
+							if (_this.length) {
+								if ((_this.frontValue + _this.backValue).length < _this.length) _this.frontValue += this.innerText
+							} else {
+								_this.frontValue += this.innerText
+							}
+					}
+					if (!/^[0-9a-zA-Z]+$/.test(_this.frontValue + _this.backValue)) {
+						_this.frontValue = ''
+						_this.backValue = ''
+					}
+					_this.$emit('change', _this.getValue())
+				}, false)
+			}
 		}
+	},
+	mounted() {
+		setTimeout(this.addEvent, 100)
+	},
+	beforeDestroy() {
+		document.getElementById('app').removeEventListener('click', this.closeKeyboard)
 	}
 }
 </script>
 
 <style lang="less" scoped>
 .keyboard-input{
-	height: 22px;
+	height: 100%;
 	width: 100%;
 	background: #fff;
-	font-size: 14px;
-	line-height: 22px;
-	padding-left: 3px;
 	color: #333;
+	position: relative;
+	z-index: 99;
+	letter-spacing: 0.5px;
+	white-space: nowrap;
 	.keyboard-cursor{
 		opacity: 0;
-		position: relative;
+		position: absolute;
 		top: -1px;
+		margin-left: -2px;
 		color: #007aff;
-		font-weight: bold;
+	}
+	img{
+		margin: 9px;
+		height: 18px;
 	}
 }
 .keyboard{
-	position: absolute;
-	height: 100%;
 	width: 100%;
-	left: 0;
-	top: 0;
 	display: none;
 	.keyboard-content{
-		position: absolute;
+		position: fixed;
+		z-index: 999999;
 		height: 200px;
 		width: 100%;
 		bottom: 0;
